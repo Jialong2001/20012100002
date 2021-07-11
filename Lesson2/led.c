1c0d934ac4121a81535062633b178ab72f94ba44
Jialong2001 <87218830+Jialong2001@users.noreply.github.com>
2021-07-11 12:55:46 +0800

    Update led.c

--- Lesson2/led.c
+++ Lesson2/led.c
@@ -1,1 +1,22 @@
+int j=13;
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
 
+void loop()
+{
+  for(j=13;j>=7;j--)
+  {
+  	digitalWrite(j, HIGH);
+  	delay(500); // Wait for 1000 millisecond(s)
+  	digitalWrite(j, LOW);
+  	delay(500); // Wait for 1000 millisecond(s)
+  }
+}
